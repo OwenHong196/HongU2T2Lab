1b18ba20eb5a51b4a2194092bd5a7a932f84905f
Owen Hong <owenh19@nycstudents.net>
2023-09-28 23:59:30 -0400

    added quadrant method and PointTester

--- src/Point.java
+++ src/Point.java
@@ -19,13 +19,28 @@ public class Point {
     public int getY(){
         return y;
     }
-    public int setX(int newX){
+    public void setX(int newX){
         x = newX;
     }
-    public int setY(int newY){
+    public void setY(int newY){
         y = newY;
     }
     public String coordinate(){
         return "(" + x + ", " + y + ")";
+    }
+    public String quadrant(){
+        if (x>0 && y>0){
+            return "I";
+        }else if (x<0 && y>0){
+            return "II";
+        }else if (x<0 && y<0){
+            return "III";
+        }else if (x>0 && y<0){
+            return "IV";
+        }else if (x==0 && y==0){
+            return "origin";
+        }else{
+            return "on an axis";
+        }
     }
 }
